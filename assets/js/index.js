35d6adb31de338bb0c965db99f72d013e95921af
li__jinrui <577507621@qq.com>
2020-09-12 20:09:31 +0800

    [有点混乱了]

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+(function () {
+  getUserInfo();
+})();
+
+function getUserInfo() {
+  $.ajax({
+    method: "GET",
+    url: "/my/userinfo",
+    headers: {
+        Authorization: localStorage.getItem('token') || ''
+      },
+    success: function(res) {
+      if (res.status !== 0) {
+        return layui.layer.msg('获取用户信息失败！')
+      }
+      // 调用 renderAvatar 渲染用户的头像
+      renderAvatar(res.data)
+    }
+  });
+}
