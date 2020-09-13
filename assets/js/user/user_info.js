d321e24ae227d0deaaebae67b0742233b9b9c463
li__jinrui <577507621@qq.com>
2020-09-13 19:54:00 +0800

    [完成了修改个人资料]

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -23,4 +23,25 @@ $(function () {
       },
     });
   }
+
+  $("#btnReset").on("click", function (e) {
+    e.preventDefault();
+    initUserInfo();
+  });
+  $(".layui-form").on("submit", function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/my/userinfo",
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg(res.message);
+        }
+        layer.msg("更新用户信息成功！");
+        // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+        window.parent.getUserInfo();
+      },
+    });
+  });
 });
