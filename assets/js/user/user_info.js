a088df9282e8019fb5de7de2c3573eba4b7e0885
li__jinrui <577507621@qq.com>
2020-09-13 18:47:18 +0800

    [基本信息的部分修改]

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,26 @@
+$(function () {
+  var form = layui.form;
+  var layer = layui.layer;
+  form.verify({
+    nickname: function (value) {
+      if (value.length > 6) {
+        return "昵称长度必须在 1 ~ 6 个字符之间！";
+      }
+    },
+  });
+  initUserInfo();
+  //获取用户基本信息
+  function initUserInfo() {
+    $.ajax({
+      method: "GET",
+      url: "/my/userinfo",
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("获取用户信息失败！");
+        }
+        console.log(res);
+        form.val("formUserInfo", res.data);
+      },
+    });
+  }
+});
